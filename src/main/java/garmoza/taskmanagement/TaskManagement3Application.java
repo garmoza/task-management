4e9065e372a28ff725e9eba978df40498418c832
Nikita Garmoza <garmozanikita@gmail.com>
2023-12-28 13:16:25 +0300

    init

---
+++ src/main/java/garmoza/taskmanagement/TaskManagement3Application.java
@@ -0,0 +1,13 @@
+package garmoza.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagement3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagement3Application.class, args);
+    }
+
+}
---
+++ src/test/java/garmoza/taskmanagement/TaskManagement3ApplicationTests.java
@@ -0,0 +1,13 @@
+package garmoza.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagement3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
