3f7adb77b9ee2fcf6991e93c967244712f53a99d
Nikita Garmoza <garmozanikita@gmail.com>
2024-01-02 17:06:01 +0300

    notValidJwt

--- src/test/java/garmoza/taskmanagement/security/service/JwtServiceTest.java
+++ src/test/java/garmoza/taskmanagement/security/service/JwtServiceTest.java
@@ -1,5 +1,7 @@
 package garmoza.taskmanagement.security.service;
 
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +10,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class JwtServiceTest {
@@ -29,5 +32,37 @@ class JwtServiceTest {
         assertThat(usernameJwt).isEqualTo(username);
         assertThat(authoritiesJwt).isEqualTo(authorities);
         assertFalse(isExpired);
+    }
+
+    @Test
+    void emptyStringJwt() {
+        String jwt = "";
+
+        assertThrows(IllegalArgumentException.class, () -> jwtService.extractUsername(jwt));
+    }
+
+    @Test
+    void notValidJwt() {
+        String username = "test-username";
+        List<String> authorities = List.of("ROLE_FIRST", "ROLE_SECOND");
+
+        String jwt = jwtService.generateTokenWithAuthorities(username, authorities);
+
+        String[] parts = jwt.split("\\.");
+
+        // creates wrong parts
+        String notValidPart1 = (parts[0].charAt(0) + 1) + parts[0].substring(1);
+        String notValidPart2 = (parts[1].charAt(1) + 1) + parts[1].substring(1);
+        String notValidPart3 = (parts[2].charAt(2) + 1) + parts[2].substring(1);
+
+        String notValid1 = notValidPart1 + "." + parts[1] + "." + parts[2];
+        String notValid2 = parts[0] + "." + notValidPart2 + "." + parts[2];
+        String notValid3 = parts[0] + "." + parts[1] + "." + notValidPart3;
+        String notValid4 = jwt + "abc";
+
+        assertThrows(MalformedJwtException.class, () -> jwtService.extractUsername(notValid1));
+        assertThrows(SignatureException.class, () -> jwtService.extractUsername(notValid2));
+        assertThrows(SignatureException.class, () -> jwtService.extractUsername(notValid3));
+        assertThrows(SignatureException.class, () -> jwtService.extractUsername(notValid4));
     }
 }
